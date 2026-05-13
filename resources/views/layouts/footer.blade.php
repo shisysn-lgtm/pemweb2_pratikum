ebbe983b928adda3a05e3ba571237c540ce19f83
alielryn <alielryn@gmail.com>
2026-05-13 09:30:06 +0700

    9_Praktikum

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="alert alert-primary text-center" role="alert">
+  <strong>Pemrograman Web 2</strong> <br>
+  <small>Semester Genap</small>
+</div>
+
+<footer class="text-center mt-4">
+  <hr>
+  <p class="mb-1">Dosen Pengampu: Pak Nasrul, S.Kom., M.Kom.</p>
+  <p class="mb-1">Nama: Ahmad Fathi Ahlul Rayan | NIM: 0110125020 | Prodi: Sistem Informasi</p>
+  <small class="text-muted">© 2026 STT Terpadu Nurul Fikri</small>
+</footer>
