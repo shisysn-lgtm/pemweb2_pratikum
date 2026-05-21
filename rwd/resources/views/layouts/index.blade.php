d8bea66ec5665585874e9afbc8b8085a583dea68
Nursyamsiah Nawawi <shisy.sn@gmail.com>
2026-05-21 21:04:45 +0700

    first commit baru

---
+++ rwd/resources/views/layouts/index.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>My Laravel</title>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" 
+         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  </head>
+  <body>  
+  <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+      @include('layouts.header')
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+      @include('layouts.menu')
+		</div>
+	</div>
+    <br/>
+	<div class="row">
+		<div class="col-md-8">
+         @if($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+      @endif
+      @if($message = Session::get('error'))
+            <div class="alert alert-danger">
+                <p>{{ $message }}</p>
+            </div>
+      @endif   
+      @yield('content')     
+		</div>
+		<div class="col-md-4">
+      @include('layouts.sidebar')
+		</div>
+	</div>
+    <br/>
+	<div class="row">
+		<div class="col-md-12">
+      @include('layouts.footer')
+		</div>
+	</div>
+  </div>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+  </body>
+</html>
